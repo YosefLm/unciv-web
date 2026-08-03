37f26d7c4288f32bc56ff3e5b7f9a5f8329cf55d
haimlm <105680265+haimlm@users.noreply.github.com>
2026-08-03 19:09:54 +0300

    web-stack 01: expose chat visibility boundary

--- core/src/com/unciv/ui/screens/worldscreen/WorldScreen.kt
+++ core/src/com/unciv/ui/screens/worldscreen/WorldScreen.kt
@@ -147,6 +147,10 @@ class WorldScreen(
 
     internal val undoHandler = UndoHandler(this)
 
+    fun refreshChatButtonVisibility() {
+        chatButton.refreshVisibility()
+    }
+
 
     init {
         // notifications are right-aligned, they take up only as much space as necessary.
